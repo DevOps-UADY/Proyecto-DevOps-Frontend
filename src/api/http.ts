b242798ecd926b2b7bc99af6f089e459ada14176
Jonatan <a17000799@alumnos.uayd.mx>
2024-04-26 13:11:07 -0600

    feat: agregando pruebas (DevOpsFrontend-005)

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = String(import.meta.env.VITE_API_URL);                  
+const baseURL = "http://localhost:3000/";               
 export const rutaURL = baseURL;
 
 export const httpClient = axios.create({
